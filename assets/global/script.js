6bf77987dd7f5241bd095c49d1dad75e7e9b52df
Gal Nagli <35578316+NagliNagli@users.noreply.github.com>
2021-06-05 02:05:21 +0300

    Update script.js

--- assets/global/script.js
+++ assets/global/script.js
@@ -1,2 +1,20 @@
-// start the particles
-particlesJS("particles-js", {"particles":{"number":{"value":160,"density":{"enable":true,"value_area":800}},"color":{"value":"#ffffff"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":5},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":1,"random":true,"anim":{"enable":true,"speed":1,"opacity_min":0,"sync":false}},"size":{"value":3,"random":true,"anim":{"enable":false,"speed":4,"size_min":0.3,"sync":false}},"line_linked":{"enable":false,"distance":150,"color":"#ffffff","opacity":0.4,"width":1},"move":{"enable":true,"speed":1,"direction":"none","random":true,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":600}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":true,"mode":"bubble"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":250,"size":0,"duration":2,"opacity":0,"speed":3},"repulse":{"distance":400,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true});
+const s = window.screen;
+const w = (q.width = s.width);
+const h = (q.height = s.height);
+const ctx = q.getContext("2d");
+
+const p = Array(Math.floor(w / 10) + 1).fill(0);
+
+const random = (items) => items[Math.floor(Math.random() * items.length)];
+
+const hex = "0123456789ABCDEF".split("");
+
+setInterval(() => {
+  ctx.fillStyle = "rgba(0,0,0,.05)";
+  ctx.fillRect(0, 0, w, h);
+  ctx.fillStyle = "#0f0";
+  p.map((v, i) => {
+    ctx.fillText(random(hex), i * 10, v);
+    p[i] = v >= h || v > 50 + 10000 * Math.random() ? 0 : v + 10;
+  });
+}, 1000 / 30);
